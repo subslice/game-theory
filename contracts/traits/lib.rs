60b86df50a842c63d03ad8adf61a82722e6d70d0
FaisalAl-Tameemi <faisal.al.tameemi@gmail.com>
2023-04-13 10:12:10 +0400

    Edit traits crate signatures; add Errors type

--- contracts/traits/lib.rs
+++ contracts/traits/lib.rs
@@ -1,8 +1,20 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use ink::primitives::AccountId;
+use ink::prelude::vec::Vec;
+use ink::storage::traits::StorageLayout;
 
-#[derive(scale::Decode, scale::Encode)]
+/// Game errors.
+#[derive(scale::Encode, scale::Decode, Debug, PartialEq, Eq)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum Error {
+    /// Caller must match the palyer being added
+    CallerMustMatchNewPlayer,
+    /// No more space for players to join
+    MaxPlayersReached
+}
+
+#[derive(scale::Decode, scale::Encode, Debug, PartialEq, Eq, Clone, Copy, StorageLayout)]
 #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
 pub enum GameStatus {
     Initialized,
@@ -11,7 +23,7 @@ pub enum GameStatus {
     Ended,
 }
 
-#[derive(scale::Decode, scale::Encode)]
+#[derive(scale::Decode, scale::Encode, Debug, PartialEq, Eq, Clone, Copy, StorageLayout)]
 #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
 pub enum RoundStatus {
     Ready,
@@ -20,7 +32,7 @@ pub enum RoundStatus {
     Ended,
 }
 
-#[derive(scale::Decode, scale::Encode)]
+#[derive(scale::Decode, scale::Encode, Debug, PartialEq, Eq, Clone, StorageLayout)]
 #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
 pub struct GameRound {
     pub round_number: u32,
@@ -48,10 +60,12 @@ pub trait GameLifecycle {
 
     /// Get the current game round.
     #[ink(message)]
-    fn get_current_round(&self) -> GameRound;
+    fn get_current_round(&self) -> Option<GameRound>;
 
     /// Adds a player into the game by their AccountId.
     /// Ensures that caller of the function has the same AccountId being added (i.e. player can add themselves).
+    /// 
+    /// Returns the number of players.
     #[ink(message, payable)]
-    fn join(&mut self, player: AccountId) -> bool;
+    fn join(&mut self, player: AccountId) -> Result<u8, Error>;
 }
