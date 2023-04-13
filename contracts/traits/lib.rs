d75864c4cf26c0b6efe0f83d3ea35c38900ff595
FaisalAl-Tameemi <faisal.al.tameemi@gmail.com>
2023-04-13 10:38:10 +0400

    Extend traits to include GameConfigs struct

--- contracts/traits/lib.rs
+++ contracts/traits/lib.rs
@@ -43,13 +43,26 @@ pub struct GameRound {
     pub total_reward: u128,
 }
 
+#[derive(scale::Decode, scale::Encode, Debug, PartialEq, Eq, Clone, StorageLayout)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub struct GameConfigs {
+    pub max_players: u8,
+    pub min_players: u8,
+    pub min_round_contribution: Option<u128>,
+    pub max_round_contribution: Option<u128>,
+    pub post_round_actions: bool,
+    /// The number of blocks before a round is considered stale.
+    pub round_timeout: Option<u32>,
+    pub max_rounds: Option<u32>,
+}
+
 /// Defines the basic game lifecycle methods.
 #[ink::trait_definition]
 pub trait GameLifecycle {
-    /// ...
-    #[ink(message, payable)]
-    fn initialize(&mut self);
-
+    /// Gets the AccountId of each each player within this instance of the game.
+    #[ink(message)]
+    fn get_configs(&self) -> GameConfigs;
+    
     /// Gets the AccountId of each each player within this instance of the game.
     #[ink(message)]
     fn get_players(&self) -> Vec<AccountId>;
