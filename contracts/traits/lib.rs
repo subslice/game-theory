08718d0a4032047010f8a7fca28f1cac4ee8cbb1
FaisalAl-Tameemi <faisal.al.tameemi@gmail.com>
2023-04-18 16:38:44 +0400

    Extend trait definition for GameLifecycle

--- contracts/traits/lib.rs
+++ contracts/traits/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use ink::primitives::AccountId;
+use ink::primitives::{AccountId, Hash};
 use ink::prelude::vec::Vec;
 use ink::storage::traits::StorageLayout;
 use scale::{Encode, Decode};
@@ -17,6 +17,12 @@ pub enum GameError {
     MaxPlayersReached,
     /// Fees paid to join the game are not sufficient
     InsufficientJoiningFees,
+    /// The round has not expired yet
+    RoundNotExpired,
+    /// No commitment made by player for the current round
+    CommitmentNotFound,
+    /// Round cannot be closed
+    FailedToCloseRound,
 }
 
 #[derive(Encode, Decode, PartialEq, Eq, Clone, Copy, Debug)]
@@ -88,4 +94,37 @@ pub trait GameLifecycle {
     /// Returns the number of players.
     #[ink(message, payable)]
     fn join(&mut self, player: AccountId) -> Result<u8, GameError>;
+
+    /// callable by any player who joined
+    /// only works once, fails on subsequent calls (since the state has changed)
+    /// emits a relevant event (all events should include some game ID for the UIs that are listening)
+    #[ink(message, payable)]
+    fn startGame(&mut self) -> Result<(), GameError>;
+
+    /// Makes a commitment to the current round by the player who called the method
+    /// The payed amount is the round contribution, to be validated based on configs
+    /// Must be recorded in the GameRound storage
+    /// emits a relevant event (should include the total # of commitments in the round, helps UI know if everyone played)
+    #[ink(message, payable)]
+    fn playRound(&mut self, commitment: Hash) -> Result<(), GameError>;
+
+    /// receives data which if hashed must match the commitment for the round made earlier
+    /// throws an error if the round has no commitment for the caller
+    /// prepares the next round if max rounds not reached
+    /// emits a relevant event
+    #[ink(message, payable)]
+    fn revealRound(&mut self, reveal: ([u8; 32], u8)) -> Result<(), GameError>;
+
+    /// claims rewards of the round (if applicable and all players have revealed)
+    /// prepares the next round
+    /// emits a relevant event
+    #[ink(message, payable)]
+    fn completeRound(&mut self) -> Result<(), GameError>;
+
+    /// succeeds only if the caller has already made a commitment
+    /// succeeds only if the round expired (passed the block timeout in config // should default to 10 or 20 blocks if None)
+    /// a penalty is incurred by the players who did not play (joining fee is not returned)
+    /// emits a relevant event
+    #[ink(message, payable)]
+    fn forceCompleteRound(&mut self) -> Result<(), GameError>;
 }
