f19cf3a372bdc641f7f8ad293890f702288fdf32
FaisalAl-Tameemi <faisal.al.tameemi@gmail.com>
2023-04-28 14:27:31 +0400

    general OpenBrush integration fixes

--- traits/lifecycle.rs
+++ traits/lifecycle.rs
@@ -1,5 +1,3 @@
-#![cfg_attr(not(feature = "std"), no_std)]
-
 use ink::primitives::{AccountId, Hash};
 use ink::prelude::vec::Vec;
 
--- traits/utils.rs
+++ traits/utils.rs
@@ -1,5 +1,3 @@
-#![cfg_attr(not(feature = "std"), no_std)]
-
 use ink::primitives::AccountId;
 use ink::prelude::vec::Vec;
 
