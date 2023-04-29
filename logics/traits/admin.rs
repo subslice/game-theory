66da882f451f370660105b9e1d08e94c31a4a983
FaisalAl-Tameemi <faisal.al.tameemi@gmail.com>
2023-04-29 17:03:41 +0400

    add ADMIN trait definition

---
+++ logics/traits/admin.rs
@@ -0,0 +1,24 @@
+use crate::traits::types::GameError;
+
+
+#[ink::trait_definition]
+pub trait GameAdmin {
+    #[ink(message)]
+    fn add_player_to_game(&mut self) -> Result<(), GameError>;
+
+    #[ink(message)]
+    fn play_round_as_player(&mut self) -> Result<(), GameError>;
+
+    #[ink(message)]
+    fn reveal_round_as_player(&mut self) -> Result<(), GameError>;
+    
+    #[ink(message)]
+    fn force_complete_round(&mut self) -> Result<(), GameError>;
+
+    #[ink(message)]
+    fn force_end_game(&mut self) -> Result<(), GameError>;
+
+    #[ink(message, payable)]
+    fn fund_contract(&self) -> Result<(), GameError>;
+}
+
